321fffd37f7fae45e2bb61b9998004e6c9a1b80d
goldenboys2011XD <167981189+goldenboys2011XD@users.noreply.github.com>
2024-08-18 16:20:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from views import LanguageDropdownView
-from config import DISCORD_TOKEN
+#from config import DISCORD_TOKEN
+
+DISCORD_TOKEN = os.getenv('token')
 
 intents = discord.Intents.default()
 intents.message_content = True
